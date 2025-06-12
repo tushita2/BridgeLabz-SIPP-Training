b59df57bd537879fc49d09743d2ffaa5e77e0b23
tushita2 <mishratushita73@gmail.com>
2025-06-12 12:17:06 +0530

    initial commit

---
+++ practicesheet_1/Ques_2.java
@@ -0,0 +1,21 @@
+//Add Two Numbers
+package practicesheet_1;
+
+public class Ques_2 {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        
+        int sum = num1 + num2;
+        
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        
+        
+    }
+    
+}
