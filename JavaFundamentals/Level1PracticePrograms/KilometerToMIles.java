28ac28e516b84a70528fbd72af949ee352093d65
tushita2 <mishratushita73@gmail.com>
2025-06-20 09:07:00 +0530

    [Tushita Mishra ]Add initial commit

---
+++ JavaFundamentals/Level1PracticePrograms/KilometerToMIles.java
@@ -0,0 +1,10 @@
+package Level1PracticePrograms;
+
+public class KilometerToMIles {
+    public static void main(String[] args) {
+        double kilometers = 10.8; // Example distance in kilometers
+        double miles = kilometers * 1.6; // Conversion factor from kilometers to miles
+        
+        System.out.printf("%.2f kilometers is equal to %.2f miles.%n", kilometers, miles);
+    }
+}
