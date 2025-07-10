9c047be467ed729689021947bc63db57193e6107
tushita2 <mishratushita73@gmail.com>
2025-07-10 09:55:17 +0530

    Tushita added MultiplicationTableArray.java

---
+++ Arrays/MultiplicationTableArray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class MultiplicationTableArray{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to print its multiplication table: ");
+        int number = sc.nextInt();
+
+        int[] table = new int[10];
+
+        for (int i = 0; i<10; i++) {
+            table[i] = number*(i+1);
+        }
+		
+        System.out.println("\nMultiplication Table of " + number + ":");
+        for (int i=0; i<10; i++) {
+            System.out.println(number + " * " + (i + 1) + " = " + table[i]);
+        }
+    }
+}
