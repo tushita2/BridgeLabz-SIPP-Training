83971048454d343828a94d73b1e3e31796dbefe5
tushita2 <mishratushita73@gmail.com>
2025-07-10 09:57:39 +0530

    Tushita added YoungestAndTallest.java

---
+++ Arrays/YoungestAndTallest.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class YoungestAndTallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        int[] heights = new int[3];
+
+        for (int i=0; i<3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+            System.out.print("Enter height (in cm) of " + names[i] + ": ");
+            heights[i] = sc.nextInt();
+        }
+
+        // Find youngest
+        int youngestIndex = 0;
+        for (int i=1; i<3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+
+        // Find tallest
+        int tallestIndex = 0;
+        for (int i=1; i<3; i++) {
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        System.out.println("\nYoungest friend is: " + names[youngestIndex] + " (Age: " + ages[youngestIndex] + ")");
+        System.out.println("Tallest friend is: " + names[tallestIndex] + " (Height: " + heights[tallestIndex] + " cm)");
+    }
+}
