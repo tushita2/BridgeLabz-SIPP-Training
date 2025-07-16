3f71e32d7402628e711834aa04819a7b543507bc
tushita2 <131939107+tushita2@users.noreply.github.com>
2025-07-16 10:20:36 +0530

    Delete JavaFundamentals directory

--- JavaFundamentals/PracticeSheet/WelcomeToBridgelabz.java
+++
@@ -1,8 +0,0 @@
-package PracticeSheet ;
-public class WelcomeToBridgelabz {
-    public static void main(String[] args) {
-       
-        System.out.println("Welcome to Bridgelabz");
-        
-    }
-}
